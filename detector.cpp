7090757870f3083c4f437065b9fd487db736048b
andrii-nagai <a.o.nagai@gmail.com>
2025-04-29 20:33:07 +0100

    work on classes

--- detector.cpp
+++ detector.cpp
@@ -1,8 +1,41 @@
-// Your First C++ Program
+// SiPM or SPAD related class
 
 #include <iostream>
+using namespace std;
+
+class Detector{
+public:
+    string Name;
+
+    Detector(){
+        Name = "TBD";
+    }
+
+    Detector(string dName){
+        Name = dName;
+    }
+
+};
+
+class SiPM:public Detector{
+
+};
+
+class SPAD:public Detector{
+
+};
+
+ostream& operator<<(ostream& COUT, Detector myDetector){
+    COUT << "Detector Name :: "<<myDetector.Name << endl;
+    return COUT;
+}
 
 int main() {
-    std::cout << "Hello World!";
+
+    Detector TestSiPM1("SiPM");
+    Detector TestSiPM2;
+    //TestSiPM.Name = "SiPM";
+
+    cout << TestSiPM1 << TestSiPM2;
     return 0;
 }
